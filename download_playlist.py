8646324a694dca0c80935835921fd7b292e0a3b0
nkilm <nikhilmohitelhs@gmail.com>
2022-01-27 19:41:04 +0530

    change download path and other changes

--- download_playlist.py
+++ download_playlist.py
@@ -1,18 +1,27 @@
 from pytube import Playlist
+from numpy import round
 
 
 def get_playlist_info(link):
     print(link.title)
+    
     # print(link.description)
     print(f"Number of videos in playlist: {link.length}\n")
     
     for video_obj in link.videos:
-        print(video_obj.streams)
-
+        for stream in video_obj.streams.filter(file_extension="mp4",progressive=True):
+            # print(type(stream))
+            print(f"{stream} -\tAudio={stream.includes_audio_track}\t{round(stream.filesize_approx/1048576,2)}MB")
+            # stream.download(output_path="./downloads")
+        print("\n")
 
 
 # playlist_link = input("Enter the link: ")
-playlist_link = "https://youtube.com/playlist?list=PLuU3eVwK0I9PT48ZBYAHdKPFazhXg76h5"
+
+playlist_link = "https://youtube.com/playlist?list=PLoMt7Ms7DgE38K2z3LXbVUBpCwXy1ODtk" # For Testing
+# playlist_link = "https://youtube.com/playlist?list=PLuU3eVwK0I9PT48ZBYAHdKPFazhXg76h5" # streamlit
+# playlist_link = "https://youtube.com/playlist?list=PL3cFiqLUjlYPGlWWszesTtdI3v0tcLe5E" # Stocks
+# playlist_link="https://youtube.com/playlist?list=PLLssT5z_DsK-h9vYZkQkYNWcItqhlRJLN" # ML
 
 playlist_obj = Playlist(playlist_link)
 
--- download_youtube_video.py
+++ download_youtube_video.py
@@ -1,20 +1,34 @@
+# https://youtu.be/tPEE9ZwTmy0 # shortest video on YT.    XD
+
 from pytube import YouTube
 from numpy import round 
 
+def completed(stream_downloaded,path):
+    print(f"Saved to {path}")
+
+def down_progress(chunk,file_handler,remaining_bytes):
+    # Segment of media file binary data, not yet written to disk.
+    print(chunk,file_handler,remaining_bytes)
+
+def get_resolutions(obj):
+    return [i.resolution for i in obj]
+
 def download_video(yt):
     print(yt.title)
+
     available_streams = yt.streams.filter(file_extension='mp4',progressive=True)
     # available_streams = yt.streams.filter(file_extension='mp4',adaptive=True)
-    
-    
-    for stream in available_streams:
-        # stream.download
-        print(f"{stream} -\tAudio={stream.includes_audio_track}\t{round(stream.filesize_approx/1048576,2)}MB")
+    print(f"Available Quality - {get_resolutions(available_streams)}")
+    vid_quality = input("Video Quality: ")
+    desired_stream = available_streams.get_by_resolution(vid_quality)
+    file_size = round(desired_stream.filesize_approx/1048576,2)
+    print(f"File Size: {file_size}MB")
+    desired_stream.download(output_path="downloads")
 
 
 link = input("Enter the link: ")
 
-yt = YouTube(link)
+yt = YouTube(link,on_complete_callback=completed)
 
 download_video(yt)
 
