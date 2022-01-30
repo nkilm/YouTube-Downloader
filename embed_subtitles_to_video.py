0f2873143b03b4eb933465856814141ec3972a8d
nkilm <nikhilmohitelhs@gmail.com>
2022-01-31 00:01:14 +0530

    caption fetch successfull: output can be seen in terminal

---
+++ embed_subtitles_to_video.py
@@ -0,0 +1,20 @@
+from pytube import Playlist,YouTube,Caption
+from os import listdir
+from numpy import array 
+
+files = array(listdir("E:\\play-list\\threeJS\\"))
+
+def getCaption(v_obj):
+    # print(v_obj.title)
+    eng_subs = v_obj.captions['a.en']
+    print(eng_subs.xml_captions)
+
+link = "https://youtu.be/uzkedMF-l4Q?list=PLbu98QxRH81KkLTN00OXhD8Y-pRVgTCnM"
+
+yt = YouTube(link)
+
+getCaption(yt)
+
+
+
+
