5f0529363c3e83c79e93df8500363ba4dbabcd65
nkilm <nikhilmohitelhs@gmail.com>
2022-01-31 13:02:27 +0530

    add .vscode to gitignore and list all available captions for a video

--- captions_using_transcript_api.py
+++ captions_using_transcript_api.py
@@ -3,20 +3,18 @@ from youtube_transcript_api.formatters import JSONFormatter
 
 link = "https://youtu.be/FK3dav4bA4s"
 
-# lang = YouTubeTranscriptApi.list_transcripts("FK3dav4bA4s")
+all_subs = YouTubeTranscriptApi.list_transcripts("FK3dav4bA4s")
 
-eng_transcript = YouTubeTranscriptApi.get_transcript("FK3dav4bA4s",languages=['en'])
+# eng_transcript = YouTubeTranscriptApi.get_transcript("FK3dav4bA4s",languages=['en'])
 
+# print(all_subs)
+eng_transcript = all_subs.find_generated_transcript(['en'])
 
-formatter = JSONFormatter()
+print(eng_transcript.fetch())
 
-# .format_transcript(transcript) turns the transcript into a JSON string.
-json_formatted = formatter.format_transcript(eng_transcript)
+# for sub in all_subs:
+    # print(sub)
 
-
-# Now we can write it out to a file.
-with open('output_subtitles.json', 'w', encoding='utf-8') as json_file:
-    json_file.write(json_formatted)
 
 
 
